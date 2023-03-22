f7a26347aba5df6bb216507a1479cafee0b06c17
Piratux <58703216+Piratux@users.noreply.github.com>
2023-03-22 11:33:17 +0200

    Add files via upload

--- app/helper.py
+++ app/helper.py
@@ -3,13 +3,10 @@ import cherrypy
 
 def error_query(error_msg = '', error_status = 400):
     cherrypy.response.status = error_status
-    return simplejson.dumps({"error": str(error_msg)})
+    return {"error": str(error_msg)}
 
 def error_query_404():
     return error_query("resource does not exist", 404)
 
 def to_json(result):
-    return simplejson.dumps(result, default=str)
-
-def from_json(result):
-    return simplejson.loads(result.decode("utf-8"))
+    return result
